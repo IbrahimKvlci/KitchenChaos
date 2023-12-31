e1b6148c4c700b888827d21f101338aecaf8c17e
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2023-12-31 15:19:21 +0300

    Player Pick up & Container Counter

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,33 +6,33 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    private ClearCounter clearCounter;
+    private IKitchenObjectParent kitchenObjectParent;
 
     public KitchenObjectSO GetKitchenObjectSO() 
     {
         return kitchenObjectSO;
     }
 
-    public ClearCounter GetClearCounter() 
+    public IKitchenObjectParent GetKitchenObjectParent() 
     { 
-        return clearCounter; 
+        return kitchenObjectParent; 
     }
 
-    public void SetClearCounter(ClearCounter clearCounter) 
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) 
     {
-        if (this.clearCounter != null)
+        if (this.kitchenObjectParent != null)
         {
-            this.clearCounter.ClearKitchenObject();
+            this.kitchenObjectParent.ClearKitchenObject();
         }
-        this.clearCounter= clearCounter;
+        this.kitchenObjectParent= kitchenObjectParent;
 
-        if (clearCounter.HasKitchenObject())
+        if (kitchenObjectParent.HasKitchenObject())
         {
-            Debug.LogError("Counter already has a KitchenObject!");
+            Debug.LogError("KitchenObjectParent already has a KitchenObject!");
         }
 
-        clearCounter.SetKitchenObject(this);
-        transform.parent=clearCounter.GetKitchenObjectFollowTransform();
+        kitchenObjectParent.SetKitchenObject(this);
+        transform.parent=kitchenObjectParent.GetKitchenObjectFollowTransform();
         transform.localPosition=Vector3.zero;
     }
 }
