8e47fd21a6f3ca730cea3c121e91f8d9660aa3be
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2024-01-01 21:16:16 +0300

    Cutting Counter

---
+++ Assets/Scripts/CuttingCounter.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //There is no kitchen object
+            if (player.HasKitchenObject())
+            {
+                //Player has a kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //Player has not a kitchen object
+            }
+        }
+        else
+        {
+            //There is a kitchen object
+            if (player.HasKitchenObject())
+            {
+                //Player has a kitchen object
+            }
+            else
+            {
+                //Player has not a kitchen object
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        if (HasKitchenObject())
+        {
+            GetKitchenObject().DestroySelf();
+            KitchenObject.SpawnKitchenObject(kitchenObjectSO, this);
+        }
+    }
+}
